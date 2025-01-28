288bf9fba9a0282a4ef95294a3926b642a47068e
ansaralikunju123 <ansarpanoor311@gmail.com>
2025-01-28 09:30:19 +0530

    in this comit i do in the signup components some improvemnts

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config();
 
 app.use(cors({
-    origin: 'http://localhost:5177',
+    origin: 'http://localhost:5173',
     credentials: true
 }));
 app.use(express.json());
