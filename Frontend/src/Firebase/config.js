c648f1ed675a7899e17065b3dd7028364c5dddb8
ansaralikunju311311 <ansarpanoor311@gmail.com>
2025-03-05 10:46:56 +0530

    in these commit i create .env files for the google auth

--- Frontend/src/Firebase/config.js
+++ Frontend/src/Firebase/config.js
@@ -1,24 +1,37 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getAnalytics } from "firebase/analytics";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAsnZcryU2VQTBXxsLjPOguNoWGVFtyWMQ",
+//   authDomain: "auth-1ee5d.firebaseapp.com",
+//   projectId: "auth-1ee5d",
+//   storageBucket: "auth-1ee5d.firebasestorage.app",
+//   messagingSenderId: "414522490713",
+//   appId: "1:414522490713:web:ca7e7fe3f1b206ae861648",
+//   measurementId: "G-QJRTNBKKZ1"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth();
+// // const analytics = getAnalytics(app);
+// export default app;
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAsnZcryU2VQTBXxsLjPOguNoWGVFtyWMQ",
-  authDomain: "auth-1ee5d.firebaseapp.com",
-  projectId: "auth-1ee5d",
-  storageBucket: "auth-1ee5d.firebasestorage.app",
-  messagingSenderId: "414522490713",
-  appId: "1:414522490713:web:ca7e7fe3f1b206ae861648",
-  measurementId: "G-QJRTNBKKZ1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
-// const analytics = getAnalytics(app);
 export default app;
