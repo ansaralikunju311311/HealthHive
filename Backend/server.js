6acbe0c838d842174adf77c2265058da47d8b261
ansaralikunju311311 <ansarpanoor311@gmail.com>
2025-04-01 22:58:00 +0530

    url

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://healthhive.ansar.sbs',
         credentials: true
     }
 });
@@ -193,7 +193,7 @@ io.on('connection', (socket) => {
 dotenv.config();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://healthhive.ansar.sbs',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     allowedHeaders:["Content-Type","Authorization","cookie"],
